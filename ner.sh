7e01cdfe12abee04be0af8d4c56fcad4846de67e
pth1993 <phamthaihoang.hn@gmail.com>
2017-07-24 15:46:25 +0700

    update shell script

--- ner.sh
+++ ner.sh
@@ -20,5 +20,5 @@ else
     wget --load-cookie cookie.txt --save-cookie cookie.txt "${url}&$c" -O "${outfile}"
     rm cookie.txt tmp
 fi
-python ner.py --word_dir ../vie-nlp/vie-nlp/embedding/words.pl --vector_dir ../vie-nlp/vie-nlp/embedding/vectors.npy --train_dir data/train.txt --dev_dir data/dev.txt --test_dir data/test.txt --num_lstm_layer 2 --num_hidden_node 64 --dropout 0.5 --batch_size 50 --patience 3
+python ner.py --word_dir embedding/words.pl --vector_dir embedding/vectors.npy --train_dir data/train.txt --dev_dir data/dev.txt --test_dir data/test.txt --num_lstm_layer 2 --num_hidden_node 64 --dropout 0.5 --batch_size 50 --patience 3
 rm out.txt
