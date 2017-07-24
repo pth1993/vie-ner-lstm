428257d16acee18c530053f70f0d6a21d14eab6f
pth1993 <phamthaihoang.hn@gmail.com>
2017-07-24 15:59:44 +0700

    update shell script

--- ner.sh
+++ ner.sh
@@ -8,10 +8,9 @@ if [ -f "$file" ]; then
 	echo "$file found."
 else
 	url="https://drive.google.com/uc?export=download&id=0BytHkPDTyLo9WU93NEI1bGhmYmc"
-    outfile=file
     wget --load-cookie cookie.txt --save-cookie cookie.txt "${url}" -O tmp
     c=`grep -o "confirm=...." tmp`
-    wget --load-cookie cookie.txt --save-cookie cookie.txt "${url}&$c" -O "${outfile}"
+    wget --load-cookie cookie.txt --save-cookie cookie.txt "${url}&$c" -O "${file}"
     rm cookie.txt tmp
 fi
 file="embedding/words.pl"
@@ -19,10 +18,9 @@ if [ -f "$file" ]; then
     echo "$file found."
 else
     url="https://drive.google.com/uc?export=download&id=0BytHkPDTyLo9SC1mRXpkbWhfUDA"
-    outfile=file
     wget --load-cookie cookie.txt --save-cookie cookie.txt "${url}" -O tmp
     c=`grep -o "confirm=...." tmp`
-    wget --load-cookie cookie.txt --save-cookie cookie.txt "${url}&$c" -O "${outfile}"
+    wget --load-cookie cookie.txt --save-cookie cookie.txt "${url}&$c" -O "${file}"
     rm cookie.txt tmp
 fi
 python ner.py --word_dir embedding/words.pl --vector_dir embedding/vectors.npy --train_dir data/train.txt --dev_dir data/dev.txt --test_dir data/test.txt --num_lstm_layer 2 --num_hidden_node 64 --dropout 0.5 --batch_size 50 --patience 3
